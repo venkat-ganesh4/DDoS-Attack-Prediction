23e6a7e6cff0c84b1bf813b0215dd7d61966c89a
venkat-ganesh4 <venkatganeshnettem1@gmail.com>
2025-07-19 11:18:54 +0530

    Add files via upload

---
+++ AMACHI~1/amachine_learning_based_classification_ddosattacks/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for amachine_learning_based_classification_ddosattacks
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amachine_learning_based_classification_ddosattacks.settings')
+
+application = get_asgi_application()
